fe8cecd5249bc85b1f834ebe7459e7c57a825d4a
Elena Williams <github@elena.net.au>
2014-03-24 12:45:38 +0800

    Added stories to urls and tidy initial.

--- firstcontrib/urls.py
+++ firstcontrib/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'firstcontrib.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('stories.urls')),
 
     (r'^ckeditor/', include('ckeditor.urls')),
     url(r'^admin/', include(admin.site.urls)),

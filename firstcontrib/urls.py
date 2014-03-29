b8bc9b870400dc933098f350e25bb2c3ec8cedab
Elena Williams <github@elena.net.au>
2014-03-29 08:42:42 +0800

    Added 'about' and 'success' TemplateViews/urls.

--- firstcontrib/urls.py
+++ firstcontrib/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.views import generic
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('stories.urls')),
+
+    url(r'^success$', generic.TemplateView.as_view(template_name="about.html"),
+                      name='about'),
+
+    url(r'^about$', generic.TemplateView.as_view(template_name="success.html"),
+                      name='story_success'),
 
     (r'^ckeditor/', include('ckeditor.urls')),
     url(r'^admin/', include(admin.site.urls)),
